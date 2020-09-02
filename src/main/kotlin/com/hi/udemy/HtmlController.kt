8969a05d6e87f3af0588c3cdd3b64fe6aeb2f245
EQ <fleflefle1991@gmail.com>
2020-09-02 20:31:19 +0900

    가입, 로그인 페이지만 만들고 DB와는 연동하지 않음. mustache문법(템플릿문법)을 이용해서 html파일에서 중복되는 header와 footer를 넣어주고 header에서는 title이 페이지별로 변경될 수 있도록 model.addAttribute("title","타이틀이름")을 넣어줌

--- src/main/kotlin/com/hi/udemy/HtmlController.kt
+++ src/main/kotlin/com/hi/udemy/HtmlController.kt
@@ -12,24 +12,30 @@ class HtmlController {
     //메소드에 URL을 맵핑 localhost:8080/URI
     //해당하는 URL이 입력될 경우 아래의 index함수가 호출됨
     //아래 index 함수의 경우 RequestMapping이 "/"이므로 메인페이지가 됨
-    @RequestMapping("/in") //RequestMapping("URL")
+    @GetMapping("/") //RequestMapping("URL")
     fun index(model : Model) : String {
-        println("index function")
+        model.addAttribute("title","HOME")
         return "index"
     }
-    //mustache를 사용하여 index메소드에서 return된 문자열이름을 가진 html파일(template폴더 안에 있는)을 연결
 
-    //GetMapping("value") get방식 요청에만 작동하는 매핑 어노테이션
-    //PathVariable
 
-    @GetMapping("/post/{num}")
-    //중괄호{ } 사이에는 변수로 취급
-    fun post(model: Model, @PathVariable num : Int) {
-    //PathVariable : 매핑 시 value값에 있는 { } 사이의 변수
-        println("num : $num")
+
+    @GetMapping("/{formType}")
+    fun htmlForm(model : Model, @PathVariable formType : String) : String {
+
+        var response = ""
+
+        if(formType.equals("sign")){
+            response = "sign"
+        } else if(formType.equals("login")) {
+            response = "login"
+        }
+
+        model.addAttribute("title",response.toUpperCase())
+
+        return response
     }
 
 
 }
 
-// xxx.mustache 확장자 형식으로 파일 작성.
